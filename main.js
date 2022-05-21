0594f56f79aa1f3ecd08af169965ff65a4070e96
Night6997 <83875915+Night6997@users.noreply.github.com>
2022-05-21 19:19:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+input="";
+stat=""
 function setup(){
 
     canvas=createCanvas(500,400);
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+
+}
+
+function start(){
+
+    ObjectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting object(s)";
+    input=document.getElementById("input1").value;
+
+}
+
+function modelLoaded(){
+
+    console.log("Model loaded");
+    stat=true;
+
+}
+
+function draw(){
+
+    image(video,0,0,500,400)
 
 }
