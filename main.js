723c4ac2fc7ffb2edc50514f920c5330823627e2
Night6997 <83875915+Night6997@users.noreply.github.com>
2022-06-01 15:32:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 input="";
 stat=""
+objects=[];
 function setup(){
 
     canvas=createCanvas(500,400);
@@ -27,5 +28,62 @@ function modelLoaded(){
 function draw(){
 
     image(video,0,0,500,400)
+    if(stat!=""){
+
+        ObjectDetector.detect(video,gotResults);
+
+        for(i=0;i<objects.length;i++){
+
+            document.getElementById("status").innerHTML="Status : Object(s) detected successfully";
+            document.getElementById("NoOfObject(s)Detected").innerHTML="Number of objects = "+objects.length;
+
+            fill("#00bfff");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            stroke("00bfff");
+            noFill();
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].y)
+
+            if(objects[0].label==input){
+
+                video.stop();
+                ObjectDetector.detect(gotResults);
+                document.getElementById("InputObjectDetected").innerHTML=input+" has been detected";
+                speak();
+        
+            }
+            else{
+    
+                document.getElementById("InputObjectDetected").innerHTML=input+" has not been detected";
+    
+            }
+
+        }
+
+    }
+
+}
+
+function gotResults(error,results){
+
+    if(error){
+
+        console.log(error);
+    }
+    else{
+
+        console.log(results);
+        objects=results;
+
+    }
+
+}
+
+function speak(){
+
+    synth=window.speechSynthesis;
+    speak_data=input+" has been detected";
+    utter_this=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter_this);
 
 }
